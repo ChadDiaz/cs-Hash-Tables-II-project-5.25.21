c4ab80a9333d2d23bdc50f10fcf0cef45f6ca7d8
ChadDiaz <chadcdiaz@gmail.com>
2021-05-25 21:01:27 -0400

    getting files set up

---
+++ csHowToComputeLoadFactor.py
@@ -0,0 +1,7 @@
+"""
+What is the proper way to compute the load factor of a has table?
+
+
+
+A = The number of items stored in a hash table divided by the total number of slots.
+"""
---
+++ csImportanceOfLoadFactor.py
@@ -0,0 +1,7 @@
+"""
+"Why is the load factor of a hash table important?"
+
+
+
+A - As the load factor of your hash table increases, so does the likelihood of a collision, which reduces your hash table's performance.  It is important to monitor the load factor and resize your hash table when the load factor gets too large. 
+"""
